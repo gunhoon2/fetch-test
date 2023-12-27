f14b75d08cc6f9923a14e4acff62c23b1d1e4830
Gunhoon Lee <gunhoon@gmail.com>
2023-12-27 09:53:57 +0900

    Add holiday info for 2024

--- tests/test__holiday.py
+++ tests/test__holiday.py
@@ -8,29 +8,30 @@ from krxfetch._holiday import holiday_from_krx
 
 def test_holiday_from_file():
     info = holiday_from_file(2022)
-
     assert len(info) == 14
     assert info[0] == '2022-01-31'
     assert info[13] == '2022-12-30'
 
     info = holiday_from_file(2023)
-
     assert len(info) == 15
     assert info[0] == '2023-01-23'
     assert info[14] == '2023-12-29'
 
     info = holiday_from_file(2024)
+    assert len(info) == 16
+    assert info[0] == '2024-01-01'
+    assert info[15] == '2024-12-31'
 
+    info = holiday_from_file(2025)
     assert len(info) == 0
 
 
 @pytest.mark.skipif(False, reason='requires http request')
 def test_holiday_from_krx():
-    info_list = holiday_from_krx(2023)
-
-    assert len(info_list) == 15
-    assert info_list[0] == '2023-01-23'
-    assert info_list[14] == '2023-12-29'
+    info = holiday_from_krx(2024)
+    assert len(info) == 16
+    assert info[0] == '2024-01-01'
+    assert info[15] == '2024-12-31'
 
 
 @pytest.mark.skipif(False, reason='requires http request')
